9d35644bf090e7269a3e0cceae3bc9bbf1326142
MichaelGyeabour <oseigyeabourm@gmail.com>
2022-12-18 23:34:56 -0500

    editted

--- pendulic.js
+++ pendulic.js
@@ -1,36 +1,36 @@
 // start : when press the submit
 const collectiveResponse = {
-    economicResponse: ["star decision", "moon fortune", "let the sun set", "don't!"],
-    socialResponse: [
-      "terrible idea",
-      "fortune",
-      "unicorn decision",
-      "go with your guts",
-      "plan out a week",
-    ],
-    legalResponse: [
-      "seek legal advice",
-      "find a pro-bono lawyer",
-      "tell the truth",
-      "thread with your rights",
-    ],
-    spiritualResponse: [
-      "pray to God, I'm not God",
-      "read your Bible daily",
-      "seek Preist Counsel",
-      "trust in God",
-    ],
-    emotionalResponse: [
-      "treat yourself",
-      "such is life",
-      "yolo",
-      "trust no one",
-    ],
-  };
+  economicResponse: [
+    "star decision",
+    "moon fortune",
+    "let the sun set",
+    "don't!",
+  ],
+  socialResponse: [
+    "terrible idea",
+    "fortune",
+    "unicorn decision",
+    "go with your guts",
+    "plan out a week",
+  ],
+  legalResponse: [
+    "seek legal advice",
+    "find a pro-bono lawyer",
+    "tell the truth",
+    "thread with your rights",
+  ],
+  spiritualResponse: [
+    "pray to God, I'm not God",
+    "read your Bible daily",
+    "seek Preist Counsel",
+    "trust in God",
+  ],
+  emotionalResponse: ["treat yourself", "such is life", "yolo", "trust no one"],
+};
 
 let button = document.querySelector(".submit");
 let input = document.querySelector(".question");
-input.addEventListener("keypress", function(event) {
+input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
     button.click();
@@ -44,26 +44,26 @@ button.addEventListener("click", () => {
   if (question) {
     let response;
     switch (window.location.pathname) {
-        case "/economic.html":
-          response = collectiveResponse.economicResponse;
-          break;
-        case "/social.html":
-            response = collectiveResponse.socialResponse;
-          break;
-        case "/legal.html":
-            response = collectiveResponse.legalResponse;
-          break;
-        case "/spiritual.html":
-            response = collectiveResponse.spiritualResponse;
-          break;
-        case "/emotional.html":
-            response = collectiveResponse.emotionalResponse;
-          break;
-        default: 
-        response =["yes", "no", "maybe"];
-          break;
-      };
-      
+      case "/economic.html":
+        response = collectiveResponse.economicResponse;
+        break;
+      case "/social.html":
+        response = collectiveResponse.socialResponse;
+        break;
+      case "/legal.html":
+        response = collectiveResponse.legalResponse;
+        break;
+      case "/spiritual.html":
+        response = collectiveResponse.spiritualResponse;
+        break;
+      case "/emotional.html":
+        response = collectiveResponse.emotionalResponse;
+        break;
+      default:
+        response = ["yes", "no", "maybe"];
+        break;
+    }
+
     let random = Math.floor(Math.random() * response.length);
     alert(response[random]);
     input.value = "";
@@ -76,6 +76,3 @@ button.addEventListener("click", () => {
 //stop
 
 console.log(window.location.pathname);
-
-
-
